005ebcdaaa83ce467c1676112a0ba7ccc6d728ca
Oleg Kabanov <oleg_kabanov@toplinesoft.com>
2007-01-20 05:15:42 +0000

    git-svn-id: file:///Users/toplinesoft/Projects/SourceDimensions/SVN-clean@55 c30a718a-ecb3-b04e-93c6-eea2d6ca4039

--- SourceDimensions/src/client/app/com/sourcedimensions/client/forms/TypeMemberDialogBase.java
+++ SourceDimensions/src/client/app/com/sourcedimensions/client/forms/TypeMemberDialogBase.java
@@ -1,6 +1,67 @@
 package com.sourcedimensions.client.forms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TypeMemberDialogBase 
 {
-
+	protected Modifier m_modifiers;
+	protected TypeMemberCategory m_categories;
+	protected Type m_type;
+	protected String m_name;
+	protected List<Parameter> m_paramList = new ArrayList<Parameter>();
+	
+	public enum TypeMemberCategory
+	{
+		FIELD(1),
+		CONSTANT(2),
+		CONSTRUCTOR(2<<1),
+		DESTRUCTOR(2<<2),
+		METHOD(2<<3),
+		PROPERTY_GET(2<<4),
+		PROPERTY_SET(2<<5),
+		EVENT_ADD(2<<6),
+		EVENT_REMOVE(2<<7),
+		INDEXER_GET(2<<8),
+		INDEXER_SET(2<<9),
+		OPERATOR(2<<10),
+		ENUM_CONST(2<<11);
+		
+		TypeMemberCategory(int val)
+		{
+			value = val;
+		}
+		
+		private final int value;
+		
+		public int value()
+		{
+			return value;
+		}		
+	}	
+	
+	public Modifier getModifier()
+	{
+		return m_modifiers;
+	}
+	
+	public TypeMemberCategory getCategory()
+	{
+		return m_categories;
+	}
+	
+	public Type getType()
+	{
+		return m_type;
+	}
+	
+	public String getName()
+	{
+		return m_name;
+	}
+	
+	public List<Parameter> getParams()
+	{
+		return m_paramList;
+	}
 }
