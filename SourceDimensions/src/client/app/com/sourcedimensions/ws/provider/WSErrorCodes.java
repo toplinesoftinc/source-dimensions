5488460c835baea30ea35a992d48a2ce327e264c
Oleg Kabanov <oleg_kabanov@toplinesoft.com>
2007-04-18 10:10:12 +0000

    git-svn-id: file:///Users/toplinesoft/Projects/SourceDimensions/SVN-clean@139 c30a718a-ecb3-b04e-93c6-eea2d6ca4039

---
+++ SourceDimensions/src/client/app/com/sourcedimensions/ws/provider/WSErrorCodes.java
@@ -0,0 +1,7 @@
+package com.sourcedimensions.ws.provider;
+
+public class WSErrorCodes 
+{
+	public static final String LOGIN_FAILED = "LOGIN_FAILED";
+	public static final String SESSION_EXPIRED = "SESSION_EXPIRED";
+}
